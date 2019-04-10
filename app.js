0d128299cc3ac6cd512497a772535eec259b168c
unknown <aanandchamp@gmail.com>
2019-04-10 14:36:41 +0530

    new

--- app.js
+++ app.js
@@ -6,18 +6,13 @@ const express    = require('express'),
 
 
 const app = express();
-//var session = require('express-session');
-const nodemailer = require('nodemailer');
-
+const request = require('request');
+var nodemailer = require("nodemailer");
 
 app.use(express.static('assets'));
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layouts', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
-
-
 
 // MONGO CLIENT CONNECT LOGIC
 mongoose.connect('mongodb://anand:omsairam123@ds135456.mlab.com:35456/pacific_printing',
@@ -118,31 +113,64 @@ app.get("/print-requirements", function(req, res){
 
 app.get("/contact", function(req, res){
   //res.render("contact")
-  var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'aanandchamp@gmail.com',
-    pass: 'omsairam@#jio'
+  nodemailer.createTestAccount((err, account) => {
+
+    // Explained as 3 steps process as I explaned in tutorial
+
+    //Step: 1 Create transporter
+    let smtpConfig = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports. For TLS use port 465
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass // generated ethereal password
+        }
+    };
+
+    let transporter = nodemailer.createTransport(smtpConfig);
+
+    //Step: 2 Setup message options
+    let mailOptions = {
+        from: 'aanandchamp@gmail.com', // sender address
+        to: 'aanandchamp@gmail.com', // list of receivers i.e [sneder1, sender2, sender3, ....]
+        subject: 'Hello ?', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    //Step: 3 Send mail using created transport
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+  });
+});
+
+app.post('/contact', function(req, res) {
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
+  {
+    return res.json({"responseError" : "Please select captcha first"});
   }
+  const secretKey = "6LdJUJ0UAAAAAOHPiNuPKQRQ-iNg0Z_XuHqESDW7";
+
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+  request(verificationURL,function(error,response,body) {
+    body = JSON.parse(body);
+
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseError" : "Failed captcha verification"});
+    }
+    res.json({"responseSuccess" : "Sucess"});
+  });
 });
-
-var mailOptions = {
-  from: 'aanandchamp@gmail.com',
-  to: 'hiteshchandwani@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-});
-
-
 app.listen(process.env.PORT || 8002, function(){
   console.log(`Pacific Printing Server Is Running`);
 });
