13b7557aaab5c76f1ff5ee1bc22a66b4734c875c
NCDesignJB <ncdesignjb@NCDesignJBsiMac.gci.net>
2019-04-28 09:09:33 -0800

    database cleanup 4/28/2019

---
+++ assets/js/printbutton.js
@@ -0,0 +1,71 @@
+function artReqButton() {
+  let printRequirements = document.getElementById('art-requirements');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
+
+function minOrderButton() {
+  let printRequirements = document.getElementById('min-order');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
+
+function setUpButton() {
+  let printRequirements = document.getElementById('setup-fees');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
+
+function turnaroundTimeButton() {
+  let printRequirements = document.getElementById('turnaround-time');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
+
+function stylesButton() {
+  let printRequirements = document.getElementById('shirt-styles');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
+
+function priceDownButton() {
+  let printRequirements = document.getElementById('price-down');
+
+  if (printRequirements.classList.contains('fa-plus')) {
+    printRequirements.classList.remove("fa-plus");
+    printRequirements.classList.add("fa-minus");
+  } else {
+    printRequirements.classList.remove("fa-minu");
+    printRequirements.classList.add("fa-plus");
+  }
+}
